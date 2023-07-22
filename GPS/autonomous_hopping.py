db423a3ab002d8769433284e96573f104571cbe0
djatjd5313 <121447232+djatjd5313@users.noreply.github.com>
2023-07-22 21:18:20 +0900

    Add files via upload

--- GPS/autonomous_hopping.py
+++ GPS/autonomous_hopping.py
@@ -6,15 +6,16 @@ from sensor_msgs.msg import NavSatFix
 import math
 import pymap3d as pm
 import numpy as np
+from queue import Queue
 #import time
 #from mechaship_interfaces.msg import Classification, ClassificationArray, Heading, DetectionArray
 #from mechaship_interfaces.srv import Key, ThrottlePercentage, RGBColor, ThrottlePulseWidth
 #from filterpy.kalman import KalmanFilter
 
-class Location(Node):
+class Hopping(Node):
     def __init__(self):
         super().__init__(
-            "gps_xy_location",
+            "autonomous_hopping",
             allow_undeclared_parameters=True,
             automatically_declare_parameters_from_overrides=True,
         )
@@ -37,10 +38,23 @@ class Location(Node):
    
         self.now_gps = [0.0,0.0]
 
-        #self.area_arr = np.zeros[3600,55]# revise go
-        self.area_gps = [0,0]
+        self.area_arr = np.zeros((3600,55))# revise go
         
         self.window =[]
+        self.goal_num = 1
+        self.status_now = 1
+        
+        
+        #goal_queue
+        self.my_queue = Queue()
+        self.queues =  [(2, 2), (3, 3), (3, 3)]
+        for element in self.queues:
+            self.my_queue.put(element)
+        #  self.my_queue.get() 
+        
+        self.goal_location = (1,1)
+        #self.goal_location = self.my_queue.get()
+        
                 
     def gps_listener_callback(self, gps):
         #self.get_logger().info('gps data: "%s"' % gps)
@@ -58,6 +72,38 @@ class Location(Node):
         x_dot= round(x*10)
         y_dot = round(y*10)
         print(x_dot,"   ",y_dot)
+        data = [# 1=up, 2=down, 3=left, 4=right
+            (1, x_dot+1, y_dot,self.area_arr[x_dot+1,y_dot], self.euclidean_distance(x_dot+1,y_dot,self.goal_location[0],self.goal_location[1])),
+            (2, x_dot-1, y_dot,self.area_arr[x_dot-1,y_dot], self.euclidean_distance(x_dot-1,y_dot,self.goal_location[0],self.goal_location[1])),
+            (3, x_dot, y_dot+1,self.area_arr[x_dot,y_dot+1], self.euclidean_distance(x_dot,y_dot+1,self.goal_location[0],self.goal_location[1])),
+            (4, x_dot, y_dot-1,self.area_arr[x_dot,y_dot-1], self.euclidean_distance(x_dot,y_dot-1,self.goal_location[0],self.goal_location[1]))
+        ]# same value -> up,down prior
+        sorted_data = sorted(data, key=lambda item: (item[3], item[4]))
+
+        closest_value = None
+        min_distance = float('inf')  
+
+        for row in sorted_data: # prior first : row[3](first go), second : distance to goal
+            if row[3] == 0:  
+                closest_value = row[0]  
+                break
+            elif row[3] == 1:  
+                # close distance select
+                if row[4] < min_distance:
+                    closest_value = row[0]
+                    min_distance = row[4]
+            else:
+                # thinking~
+                return 0       
+        self.area_arr[x_dot,y_dot] = 1 # passed route = 1
+        
+        self.go_goal(closest_value)
+        
+        return 0
+        
+            # if find index == negative  - code gogo
+   
+        
     
     def gps_enu_converter(self,gnss):
         e, n, u = pm.geodetic2enu(gnss[0], gnss[1], gnss[2], self.origin[0], self.origin[1], self.origin[2])
@@ -82,9 +128,33 @@ class Location(Node):
         filtered_data = [sum(col) / len(col) for col in zip(*self.window)]
         return filtered_data
     
+    def go_goal(self,direction):
+        # 1
+        if(direction == 1):
+            #yaw = 0 change go 10cm
+            return 0
+        elif(direction == 2):
+            #yaw = 180 change go 10cm
+            return 0
+        elif(direction == 3):
+            #yaw = 90 change go 10cm
+            return 0
+        elif(direction ==4):
+            #yaw = -90 change go 10cm
+            return 0
+            
+        return 0    
+    
+    
+    def euclidean_distance(self,x1, y1, x2, y2):
+        
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)         
+    
+     
+    
 def main(args=None):
     rclpy.init(args=args)
-    node = Location()
+    node = Hopping()
     try:
         rclpy.spin(node)
 
