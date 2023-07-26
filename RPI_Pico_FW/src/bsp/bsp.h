ae819112266625bbf9663a67609c4a0214b2c822
Icetea0113 <v2msyo@naver.com>
2023-07-27 01:05:23 +0900

    delete RPI_Pico_FW

--- RPI_Pico_FW/src/bsp/bsp.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __BSP_H__
-#define __BSP_H__
-
-#include "battery/include/battery.h"
-#include "neopixel/include/neopixel.h"
-#include "throttle/include/throttle.h"
-
-#endif
--- RPI_Pico_FW/src/modules/include/error_loop.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MODULES_ERROR_LOOP_H__
-#define __MODULES_ERROR_LOOP_H__
-
-#include <rcl/rcl.h>
-
-void error_loop(rcl_ret_t error, const char *file_name_, const char *function_name_, int line_cnt_);
-
-#endif
