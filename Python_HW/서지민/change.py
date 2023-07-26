82b5c9eb8ba2b0d910326611fd7352351f85456e
Icetea0113 <v2msyo@naver.com>
2023-07-27 01:13:33 +0900

    delete Python_HW

--- Python_HW/서지민/change.py
+++
@@ -1,9 +0,0 @@
-mon = list(map(int, input().split()))
-money = int(input())
-
-na = [] 
-for i in range (0, len(mon)):
-    na.append(int(money//mon[i]))
-    money = money % mon[i]
-
-print(' '.join(str(_) for _ in na))
