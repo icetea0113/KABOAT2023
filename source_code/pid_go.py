f85e33d39ff4fa15bc7c17985bd291e80ae1a58e
djatjd5313 <121447232+djatjd5313@users.noreply.github.com>
2023-07-29 12:24:55 +0900

    Add files via upload

--- source_code/pid_go.py
+++ source_code/pid_go.py
@@ -10,7 +10,7 @@ import pymap3d as pm
 #import numpy as np
 #from queue import Queue
 #import time
-#from mechaship_interfaces.msg import  Heading
+from mechaship_interfaces.msg import  Heading
 #from mechaship_interfaces.srv import Key, ThrottlePercentage, RGBColor, ThrottlePulseWidth
 #from filterpy.kalman import KalmanFilter
 
@@ -31,39 +31,115 @@ class MotorControlNode(Node):
         self.subscription = self.create_subscription(NavSatFix, "fix", self.gps_listener_callback, qos_profile)
         self.subscription
         
-        self.motor_pid = PID(30, 0, 0.05)# 3.3m를 기준으로 설계
+        self.subscription = self.create_subscription(Heading, "heading", self.heading_listener_callback, qos_profile)
+        self.subscription        
+        
+        self.motor_pid = PID(30, 0, 0.05)# 3.3m를 기준으로 설계, D항은 실험을 통해 0으로 시작하여 점차 늘리며 거리별로 계산해둔다.
         self.motor_pid.output_limits = (0, 100) # 출력 범위 제한
         self.current_position = None
         self.target_position = None
         
         self.angle_pid = PID(1.0/3, 0, 0.05)# 3.3m를 기준으로 설계
-        self.motor_pid.output_limits = (0, 60) # 출력 범위 제한(임의)
-        self.current_anlge = None
+        self.angle_pid.output_limits = (0, 60) # 출력 범위 제한(임의)
+        self.current_angle = None
         self.target_angle = None        
- 
+
+        self.pid_status = 0 # 0 : 목표지정, 1: 각도 pid, 2: 거리 pid
+
+        self.now_heading = 0.0
+        #pid 시작할 때 방향과 갯수
+        self.num = 0
+        self.diff = 1
+        self.before_diff = 1
+        #pid 시작할 때 현재 노드
+        self.curr_x = None
+        self.curr_y = None
+        
+    # 큐에서 방향 꺽일 때를 기준으로 목표 이동 거리 및 방향 선택
  
     # 직진 할 때 거리 제어
-    def pid_distance(self):
-        if self.target_position is not None and self.current_position is not None:
-            self.motor_pid.setpoint = self.target_position
-            error = self.target_position - self.current_position
-            motor_speed = self.motor_pid(error)
+    def pid_distance(self,y,x):
+        
+
+           
+        #목표 지점 정하기 (한번만 하면 돼서, 수정해주면 좋을 거 같다), status가 0에서 1로 바뀌며 diff, num 넘겨 줄 때 같이 해주면 될듯
+        if(self.current_position == None):
+            if(self.diff == 1):
+                self.target_position = self.curr_y + self.num
+            elif(self.diff == 4):
+                self.target_position = self.curr_y - self.num
+            elif(self.diff == 2):
+                self.target_position = self.curr_x + self.num
+            elif(self.diff == 3):
+                self.target_position = self.curr_x - self.num
+            
+        if(self.diff ==1 or self. diff == 4):
+            self.current_position = y
+        else:
+            self.current_position = x
+            
+                
+        self.motor_pid.setpoint = self.target_position           
+        motor_speed = self.motor_pid(self.current_position)
             # motor_speed의 비율로 양 모터 같은 출력 나오게 채택
             # 같은 출력은 수조에서 실험후 기입
-    # 방향 틀 때 각도 제어        
+
+        if(error<3.0):# error 정의하기
+            self.pid_status=0
+            self.before_diff = self.diff
+            self.current_position = None
+            
+            #pid 다 썼으면 초기화 해두기
+            self.motor_pid = PID(30, 0, 0.05)# 3.3m를 기준으로 설계
+            self.motor_pid.output_limits = (0, 100)
+            self.angle_pid = PID(1.0/3, 0, 0.05)# 3.3m를 기준으로 설계
+            self.angle_pid.output_limits = (0, 60) # 출력 범위 제한(임의)                    
+           
     def pid_angle(self):
         #시계방향으로 돌릴지 반시계방향으로 돌릴지 판단 코드
         #시작 각도와 목표 각도 잡고 pid 제어
         #모터를 반대 방향으로 같은 크기로 돌리는데 그 크기를 각도 오차를 통해서 pid 제어
         #이때는 비례상수가 최대 차이 180도 일 때 임의의 같은 출력(ex 60출력) -> 비례상수 1/3수준으로 지정
-        
-        pass
+
+        if(self.diff == 1):
+            self.target_angle = 0.0
+        elif(self.diff ==4):
+            self.target_angle = 180.0
+        elif(self.diff ==2):
+            self.target_angle =-90.0
+        else:
+            self.target_angle=90.0
+        error = self.angle_difference(self.now_heading, self.target_angle)    
+        self.angle_pid.setpoint = 0
+           
+        #error가 양수이면 시계방향 회전, 음수이면 반시계방향 회전                
+        motor_speed = self.angle_pid(error)
+        #if(error>0):
+        #   역방향 추진    
+            
+        if(error<3.0):# error 정의하기
+            self.pid_status=2
+            
+               
+    #누적 오차로 예상 이외의 점으로 가는거 괜찮나?
     
+    def heading_listener_callback(self, data):
+        self.now_heading = data.yaw
+
+        if(self.pid_status == 1):
+            
+            # 후진 코드 만들기
+            
+            self.pid_angle()
+        
     def gps_listener_callback(self, gps):
         #self.get_logger().info('gps data: "%s"' % gps)
         e, n= self.gps_enu_converter([gps.latitude, gps.longitude, gps.altitude])
-        x, y = self.get_xy(e,n)    
-    
+        y, x = self.get_xy(e,n)# 경기장 위쪽을 y로 하겠다.
+
+        if(self.pid_status ==2):
+            self.pid_distance(y,x)
+              
     def gps_enu_converter(self,gnss):
         e, n, u = pm.geodetic2enu(gnss[0], gnss[1], gnss[2], self.origin[0], self.origin[1], self.origin[2])
         return e, n
@@ -73,7 +149,10 @@ class MotorControlNode(Node):
         x = e * math.cos(angle) + n * math.sin(angle)
         y = -e * math.sin(angle) + n * math.cos(angle)
         return x, y
-        
+    
+    def angle_difference(a, b):
+        diff = (b - a + 180) % 360 - 180
+        return diff    
        
     
 def main(args=None):
